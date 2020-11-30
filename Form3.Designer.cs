7ac29e175c9d58d2907712ecf45eabfd02530b41
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-11-30 20:29:06 +0200

    Φόρμα Προσθήκης ερώτησης σε εξέλιξη

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,22 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.buttonAddQuestion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonConfirmAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(966, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuestion.Location = new System.Drawing.Point(12, 37);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(80, 19);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "ΕΡΩΤΗΣΗ";
+            // 
+            // buttonAddQuestion
+            // 
+            this.buttonAddQuestion.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonAddQuestion.BackColor = System.Drawing.Color.Green;
+            this.buttonAddQuestion.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddQuestion.ForeColor = System.Drawing.Color.White;
+            this.buttonAddQuestion.Location = new System.Drawing.Point(12, 508);
+            this.buttonAddQuestion.Name = "buttonAddQuestion";
+            this.buttonAddQuestion.Size = new System.Drawing.Size(1180, 43);
+            this.buttonAddQuestion.TabIndex = 2;
+            this.buttonAddQuestion.Text = "ΠΡΟΣΘΗΚΗ ΕΡΩΤΗΣΗΣ ΣΤΗΝ ΤΡΑΠΕΖΑ";
+            this.buttonAddQuestion.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ΚΕΦΑΛΑΙΟ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(309, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 217);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(339, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(163, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Απάντηση 1";
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Font = new System.Drawing.Font("Roboto Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPrev.Location = new System.Drawing.Point(16, 217);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(23, 23);
+            this.buttonPrev.TabIndex = 7;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Roboto Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(390, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 24);
+            this.button1.TabIndex = 8;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonConfirmAnswer
+            // 
+            this.buttonConfirmAnswer.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonConfirmAnswer.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConfirmAnswer.Location = new System.Drawing.Point(45, 246);
+            this.buttonConfirmAnswer.Name = "buttonConfirmAnswer";
+            this.buttonConfirmAnswer.Size = new System.Drawing.Size(339, 23);
+            this.buttonConfirmAnswer.TabIndex = 9;
+            this.buttonConfirmAnswer.Text = "Επιβεβαίωση Απάντησης";
+            this.buttonConfirmAnswer.UseVisualStyleBackColor = false;
             // 
             // FormAddQuestion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1204, 563);
+            this.Controls.Add(this.buttonConfirmAnswer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAddQuestion);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormAddQuestion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Προσθήκη Νέου Θέματος";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button buttonAddQuestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonConfirmAnswer;
     }
 }
