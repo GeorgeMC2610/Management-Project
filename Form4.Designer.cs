940a854eff108570f956f0597d2abb41300958c9
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-12-18 14:12:25 +0200

    Αλλαγή της πρόσοψης της 4ης Φόρμας

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -30,20 +30,28 @@
         {
             this.buttonGenerateWordFile = new System.Windows.Forms.Button();
             this.richTextBoxToWord = new System.Windows.Forms.RichTextBox();
-            this.labelQuestions = new System.Windows.Forms.Label();
-            this.numericUpDownQuestions = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.numericUpDownMaxAnswers = new System.Windows.Forms.NumericUpDown();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxChapterSelect = new System.Windows.Forms.TextBox();
             this.buttonNext = new System.Windows.Forms.Button();
             this.buttonPrev = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuestions)).BeginInit();
+            this.buttonIncludeAllChapters = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxAnswers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonGenerateWordFile
@@ -70,75 +78,22 @@
             this.richTextBoxToWord.Text = "";
             this.richTextBoxToWord.Visible = false;
             // 
-            // labelQuestions
-            // 
-            this.labelQuestions.AutoSize = true;
-            this.labelQuestions.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelQuestions.Location = new System.Drawing.Point(51, 29);
-            this.labelQuestions.Name = "labelQuestions";
-            this.labelQuestions.Size = new System.Drawing.Size(168, 19);
-            this.labelQuestions.TabIndex = 2;
-            this.labelQuestions.Text = "Αριθμός Ερωτήσεων";
-            // 
-            // numericUpDownQuestions
-            // 
-            this.numericUpDownQuestions.Location = new System.Drawing.Point(56, 55);
-            this.numericUpDownQuestions.Name = "numericUpDownQuestions";
-            this.numericUpDownQuestions.Size = new System.Drawing.Size(63, 22);
-            this.numericUpDownQuestions.TabIndex = 3;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 104);
+            this.label1.Location = new System.Drawing.Point(51, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(292, 19);
             this.label1.TabIndex = 4;
             this.label1.Text = "Συμπερίληψη Ερωτήσεων Δυσκολίας";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(56, 126);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(77, 20);
-            this.checkBox1.TabIndex = 5;
-            this.checkBox1.Text = "Εύκολης";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Checked = true;
-            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox2.Location = new System.Drawing.Point(56, 152);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(73, 20);
-            this.checkBox2.TabIndex = 6;
-            this.checkBox2.Text = "Μέτριας";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Checked = true;
-            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox3.Location = new System.Drawing.Point(56, 178);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(85, 20);
-            this.checkBox3.TabIndex = 7;
-            this.checkBox3.Text = "Δύσκολης";
-            this.checkBox3.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(51, 228);
+            this.label2.Location = new System.Drawing.Point(51, 160);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(203, 19);
             this.label2.TabIndex = 8;
@@ -163,19 +118,20 @@
             this.numericUpDownMaxAnswers.Size = new System.Drawing.Size(63, 22);
             this.numericUpDownMaxAnswers.TabIndex = 10;
             // 
-            // textBox1
+            // textBoxChapterSelect
             // 
-            this.textBox1.Location = new System.Drawing.Point(55, 251);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 22);
-            this.textBox1.TabIndex = 11;
+            this.textBoxChapterSelect.Location = new System.Drawing.Point(55, 183);
+            this.textBoxChapterSelect.Name = "textBoxChapterSelect";
+            this.textBoxChapterSelect.ReadOnly = true;
+            this.textBoxChapterSelect.Size = new System.Drawing.Size(288, 22);
+            this.textBoxChapterSelect.TabIndex = 11;
             // 
             // buttonNext
             // 
             this.buttonNext.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonNext.Location = new System.Drawing.Point(349, 251);
+            this.buttonNext.Location = new System.Drawing.Point(349, 182);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(23, 23);
+            this.buttonNext.Size = new System.Drawing.Size(23, 24);
             this.buttonNext.TabIndex = 12;
             this.buttonNext.Text = ">";
             this.buttonNext.UseVisualStyleBackColor = true;
@@ -183,38 +139,145 @@
             // buttonPrev
             // 
             this.buttonPrev.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPrev.Location = new System.Drawing.Point(26, 251);
+            this.buttonPrev.Location = new System.Drawing.Point(25, 182);
             this.buttonPrev.Name = "buttonPrev";
-            this.buttonPrev.Size = new System.Drawing.Size(23, 23);
+            this.buttonPrev.Size = new System.Drawing.Size(24, 24);
             this.buttonPrev.TabIndex = 13;
             this.buttonPrev.Text = "<";
             this.buttonPrev.UseVisualStyleBackColor = true;
+            // 
+            // buttonIncludeAllChapters
+            // 
+            this.buttonIncludeAllChapters.BackColor = System.Drawing.Color.Green;
+            this.buttonIncludeAllChapters.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIncludeAllChapters.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonIncludeAllChapters.Location = new System.Drawing.Point(55, 261);
+            this.buttonIncludeAllChapters.Name = "buttonIncludeAllChapters";
+            this.buttonIncludeAllChapters.Size = new System.Drawing.Size(288, 49);
+            this.buttonIncludeAllChapters.TabIndex = 15;
+            this.buttonIncludeAllChapters.Text = "Να συμπεριληφθούν όλων των ερωτήσεων από όλα τα κεφάλαια";
+            this.buttonIncludeAllChapters.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown1.Location = new System.Drawing.Point(55, 51);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(124, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Εύκολων";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown2.Location = new System.Drawing.Point(55, 79);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown2.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(124, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Μέτριων";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown3.Location = new System.Drawing.Point(55, 107);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown3.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Δύσκολων";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown4.Location = new System.Drawing.Point(55, 211);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown4.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(124, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(201, 16);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Ερωτήσεις από αυτό το κεφάλαιο";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(552, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(172, 16);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "(Max.) Απαντήσεις ανά θέμα";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1219, 545);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonIncludeAllChapters);
             this.Controls.Add(this.buttonPrev);
             this.Controls.Add(this.buttonNext);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxChapterSelect);
             this.Controls.Add(this.numericUpDownMaxAnswers);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDownQuestions);
-            this.Controls.Add(this.labelQuestions);
             this.Controls.Add(this.richTextBoxToWord);
             this.Controls.Add(this.buttonGenerateWordFile);
             this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form4";
-            this.Text = "Form4";
+            this.Text = "Παραγωγή Αρχείου Word";
             this.Load += new System.EventHandler(this.Form4_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuestions)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMaxAnswers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,17 +287,22 @@
 
         private System.Windows.Forms.Button buttonGenerateWordFile;
         private System.Windows.Forms.RichTextBox richTextBoxToWord;
-        private System.Windows.Forms.Label labelQuestions;
-        private System.Windows.Forms.NumericUpDown numericUpDownQuestions;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown numericUpDownMaxAnswers;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxChapterSelect;
         private System.Windows.Forms.Button buttonNext;
         private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonIncludeAllChapters;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
